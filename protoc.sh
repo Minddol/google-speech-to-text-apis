f1116621cefa223fa9d79cb118c29e4ddc37f456
Minddol <minddol@gmail.com>
2023-05-02 00:02:47 +0900

    grpc swift out

---
+++ protoc.sh
@@ -0,0 +1,1 @@
+protoc --swift_out=Sources/SwiftGoogleApis --grpc-swift_out=Sources/SwiftGoogleApis  --swift_opt=Visibility=public --grpc-swift_opt=Visibility=Public google/*/*.proto google/*/*/*/*.proto
